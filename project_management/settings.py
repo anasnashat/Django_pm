319f65be7dced151095390b794cf18930b5eae6f
anasnashat <72121781+anasnashat@users.noreply.github.com>
2022-09-16 05:07:31 +0200

    Heroku setup

--- project_management/settings.py
+++ project_management/settings.py
@@ -133,11 +133,12 @@ mimetypes.add_type("application/javascript", ".js", True)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #this is not used
+# Add static folder to STATIC_DIRS
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
